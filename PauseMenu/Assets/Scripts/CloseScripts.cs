74ee5e1e713dba302b1207919b6d2738641af070
Sh-Revo <shandov@i.ua>
2022-05-15 22:27:36 +0300

    Create full menu

---
+++ PauseMenu/Assets/Scripts/CloseScripts.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CloseScripts : MonoBehaviour
+{
+    [SerializeField] private Button _closeButton;
+    [SerializeField] private GameObject pauseMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _closeButton.onClick.AddListener(OnCloseButtonHandler);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCloseButtonHandler()
+    {
+        Debug.Log("[OnCloseButtonHandler] OK");
+        pauseMenu.SetActive(false);
+    }
+}
---
+++ PauseMenu/Assets/Scripts/LogPressButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LogPressButton : MonoBehaviour
+{
+    [SerializeField] private Button _button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _button.onClick.AddListener(OnPressButtonHandler);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnPressButtonHandler()
+    {
+        Debug.Log("[OnPressButtonHandler] OK, " + _button.name);
+    }
+}
